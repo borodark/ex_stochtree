a8fd2daf82127bbd97ed9829ff4288c46b363a19
Igor Ostaptchenko <igor@octanix.com>
2026-03-24 09:57:00 -0400

    Initial mix new scaffold

---
+++ lib/stoch_tree.ex
@@ -0,0 +1,18 @@
+defmodule StochTree do
+  @moduledoc """
+  Documentation for `StochTree`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> StochTree.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/stoch_tree_test.exs
@@ -0,0 +1,8 @@
+defmodule StochTreeTest do
+  use ExUnit.Case
+  doctest StochTree
+
+  test "greets the world" do
+    assert StochTree.hello() == :world
+  end
+end
